f1074caab01f0a5be4949cfdc58879d6cd084345
Nick Marshall <marshallnw18@mail.vmi.edu>
2017-12-27 02:57:50 -0500

    V1.2

--- app/src/main/java/com/example/marshallnw18/wilksv2/UserFragment.java
+++ app/src/main/java/com/example/marshallnw18/wilksv2/UserFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 
@@ -64,7 +66,19 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        //Inflated layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+
+        //Creating Gender Spinner for gender selection
+        Spinner genderSpinner = (Spinner) view.findViewById(R.id.genderspinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.gender_options, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        genderSpinner.setAdapter(adapter);
+
         return inflater.inflate(R.layout.fragment_user, container, false);
     }
 
