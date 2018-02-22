a0fd7a44613d4f4d490e1e2c2ac3adb0c4c669dc
marshallnw18 <marshallnw18@mail.vmi.edu>
2018-02-21 23:51:55 -0500

    V1.52 Updated UI/UX

---
+++ app/src/main/java/com/example/marshallnw18/virtus/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.marshallnw18.virtus;
+
+/**
+ * Created by marshallnw18 on 2/21/2018.
+ */
+
+import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ssaurel on 02/12/2016.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
